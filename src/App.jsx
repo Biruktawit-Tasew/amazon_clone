c820ed779aaff6aa0717ad6cd2bc8072b4e21d7d
Biruktawit-Tasew <birukti.tasew@gmail.com>
2025-02-19 12:20:48 -0300

    category

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import CarouselEffect from './components/Carousel/CarouselEffect'
+import Category from './components/Category/Category'
 import Header from './components/Header/Header'
+import Product from './components/Product/Product'
 
 function App() {
   return (
     <>
     <Header/>
  <CarouselEffect />
+< Category/>
+<Product/>
     </>
   )
 }
---
+++ src/components/Category/categoryData.js
@@ -0,0 +1,24 @@
+const categoryData = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's Clothing",
+    imgLink: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+  },
+  {
+    title: "Jewelery",
+    name: "jewelery",
+    imgLink: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+  },
+  {
+    title: "Women's clothing",
+    name: "women's clothing",
+    imgLink: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+  },
+];
+
+export default categoryData
