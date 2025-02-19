3b503bcd0e61fb7e2549120c2cf8b07472f4c15d
Biruktawit-Tasew <birukti.tasew@gmail.com>
2025-02-19 09:05:42 -0300

    carouselEffect component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import CarouselEffect from './components/Carousel/CarouselEffect'
 import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
     <Header/>
- 
+ <CarouselEffect />
     </>
   )
 }
